71512486f0658b05e1358ee2bc0a706401fae078
yzcccccccccc <yangzechen21@mails.ucas.ac.cn>
2024-02-28 22:02:00 +0800

    finish a raw version of npc Makefile

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include "Vtop.h"
+#include "verilated.h"
+
+#include "verilated_vcd_c.h"
+
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+
+
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    contextp->traceEverOn(true);
+    top->trace(tfp, 0); 
+    tfp->open("wave.vcd");
+
+    int i=20;
+    while (!contextp->gotFinish() && i>=0) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+
+        assert(top->f == (a^b));
+
+        i--;
+    }
+    delete top;
+    tfp->close();
+    delete contextp;
+    return 0;
 }
