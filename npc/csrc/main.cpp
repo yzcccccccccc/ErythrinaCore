ed7967a19fa5f75ab6dc8391cdcfbc2eeff5f282
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-03-04 21:38:28 +0800

    pass light example!

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -11,18 +11,19 @@
 
 #ifdef NVBOARD
 #include <nvboard.h>
-#include <Vtop.h>
 
 static TOP_NAME dut;
 void nvboard_bind_all_pins(TOP_NAME* top);
 
-static void single_cycle() {
-  dut.a = rand() & 1;
-  dut.b = rand() & 1;
-  dut.eval();
-  dut.a = rand() & 1;
-  dut.b = rand() & 1;
-  dut.eval();
+void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
 }
 
 #endif
@@ -39,21 +40,24 @@ int main(int argc, char** argv) {
     top->trace(tfp, 0); 
     tfp->open("wave.vcd");
 
-    int i=20;
-    while (!contextp->gotFinish() && i>=0) {
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    top->clk = 0;
+    top->rst = 1;
 
+    // Reset
+    for (int i = 0; !contextp->gotFinish() && i < 10; i++){
+        top->eval();
+        top->clk = !top->clk;
         tfp->dump(contextp->time());
         contextp->timeInc(1);
+    }
+    top->rst = 0;
 
-        assert(top->f == (a^b));
-
-        i--;
+    // Work
+    for (int i = 0; !contextp->gotFinish() && i < 100; i++){
+        top->eval();
+        top->clk = !top->clk;
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
     }
     delete top;
     tfp->close();
@@ -63,7 +67,7 @@ int main(int argc, char** argv) {
 #ifdef NVBOARD
     nvboard_bind_all_pins(&dut);
     nvboard_init();
-
+    reset(10);
 
     while(1) {
         nvboard_update();
