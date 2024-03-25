a8c914ce65980e4551298b2a44e9612778769d38
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-03-25 11:31:03 +0800

    add difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,9 +16,21 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
+#include "debug.h"
 
+// ref_r is the regs in Standard Machine (qemu, spike, etc.)
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++){
+    if (ref_r->gpr[i] != cpu.gpr[i]){
+      Log("[difftest:0x%x] Reg %s error, ref_r: 0x%x, nemu: 0x%x", pc, reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
+      return false;
+    }
+  }
+  if (ref_r->pc != cpu.pc){
+    Log("[difftest:0x%x] NPC error, ref_r pc: 0x%x, nemu pc: 0x%x", pc, ref_r->pc, cpu.pc);
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
