fe06d0b0bfe2a906bfe53463a4ddbad3ad84367f
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-04-19 22:55:59 +0800

    pass dummy for ysyxsoc

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -12,7 +12,7 @@ CFLAGS	+= -fdata-sections -ffunction-sections
 LDFLAGS	+= -T $(AM_HOME)/scripts/ysyxsoc-linker.ld \
 			--defsym=_mrom_start=0x20000000 --defsym=_entry_offset=0x0 --defsym=_stack_top=0x0f000000
 LDFLAGS += --gc-sections -e _start
-CFLAGS	+= -DMAINARGS=\"$(mainargs)"\
+CFLAGS	+= -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/ysyx/trm.c
 
 NPC_ARGS ?= 
