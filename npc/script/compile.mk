a849553416368e3dcfd04054d291f54ea9c61e2e
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-05-09 10:27:50 +0800

    update script

--- npc/script/compile.mk
+++ npc/script/compile.mk
@@ -1,5 +1,6 @@
 # For Erythcore Compilation
 RTL_DIR		= $(BUILD_DIR)/rtl
+$(shell mkdir -p $(RTL_DIR))
 
 SCALA_FILES = $(shell find src/vsrc/ -name "*.scala")
 V_FILE_TAR	= $(RTL_DIR)/$(YSYXNAME).sv
@@ -11,7 +12,6 @@ test:
 	mill -i __.test
 
 $(V_FILE_TAR):	$(SCALA_FILES)
-	-@ mkdir $(RTL_DIR)
 	$(call git_commit, "generate verilog")
 	mill -i __.test.runMain Elaborate --target-dir $(RTL_DIR) $(MILL_ARGS_ALL)
 	-@ sed -i -e 's/_\(master\|slave\)_\(aw\|ar\|w\|r\|b\)_\(\|bits_\)/_\1_\2/g' $(RTL_DIR)/$(YSYXNAME).sv
--- npc/script/yosys.mk
+++ npc/script/yosys.mk
@@ -2,9 +2,10 @@
 YOSYSTA_PATH = ../yosys-sta
 
 RTL_STA_DIR = $(BUILD_DIR)/sta-rtl
+$(shell mkdir -p $(RTL_STA_DIR))
+
 V_FILE_TIME = $(RTL_STA_DIR)/TimingTop.sv
 $(V_FILE_TIME): $(SCALA_FILES)
-	-@ mkdir $(RTL_STA_DIR)
 	$(call git_commit, "generate verilog")
 	mill -i __.test.runMain Elaborate_Time --target-dir $(RTL_STA_DIR) $(MILL_ARGS_ALL)
 
@@ -18,5 +19,5 @@ RESULT_BASE = $(abspath $(BUILD_DIR))
 
 sta: $(V_FILE_TIME)
 	@echo $(RTL_FILE)
-	@$(MAKE) -C $(YOSYSTA_PATH) sta RES_PATH=$(RES_PATH) DESIGN=TimingTop SDC_FILE=$(SDC_FILE) RTL_FILES="$(RTL_FILE)" CLK_FREQ_MHZ=200 RESULT_BASE_DIR=$(RESULT_BASE)
+	@$(MAKE) -C $(YOSYSTA_PATH) sta RES_PATH=$(RES_PATH) DESIGN=TimingTop SDC_FILE=$(SDC_FILE) RTL_FILES="$(RTL_FILE)" CLK_FREQ_MHZ=300 RESULT_BASE_DIR=$(RESULT_BASE)
 
