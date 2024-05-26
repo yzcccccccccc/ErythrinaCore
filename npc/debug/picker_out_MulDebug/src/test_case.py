9e3f9fbf0a4106181f55172b2c0123f6fc19408a
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-05-26 12:42:33 +0800

    update ut-test

--- npc/debug/picker_out_MulDebug/src/test_case.py
+++ npc/debug/picker_out_MulDebug/src/test_case.py
@@ -11,34 +11,36 @@ async def mul_test(dut):
 
     mlvp.create_task(mlvp.start_clock(dut))
     print('\n')
-    for i in range(10):
-        op  = random.randint(0, 3)
+    for i in range(30):
+        op  = 0
         a_s = 1 if op != 3 else 0               # signed or unsigned
         b_s = 1 if (op >> 1) == 0 else 0        # signed or unsigned
 
         a_src = None
         if (a_s == 0):
-            a_src = np.uint32(random.randint(0, 2**32-1))
+            a_src = np.uint32(random.randint(0, 32))
         else:
-            a_src = np.int32(random.randint(-2**31, 2**31-1))
+            a_src = np.int32(random.randint(-32, 32))
 
         b_src = None
         if (b_s == 0):
-            b_src = np.uint32(random.randint(0, 2**32-1))
+            b_src = np.uint32(random.randint(0, 32))
         else:
-            b_src = np.int32(random.randint(-2**31, 2**31-1))
+            b_src = np.int32(random.randint(-32, 32))
 
         dut.io_a.value  = int(a_src)
         dut.io_b.value  = int(b_src)
         dut.io_op.value = op
         dut.io_v.value  = 1
-        await mlvp.ClockCycles(dut, 2)
+        await mlvp.ClockCycles(dut, 1)
+        dut.io_v.value  = 0
+        await mlvp.ClockCycles(dut, 10)
 
         res = dut.io_res.value
         expected = np.int64(a_src) * np.int64(b_src)
 
         # check
-        print(f"op: {op}, a: {a_src}, b: {b_src}, res: {res}, expected: {expected}")
+        print(f"op: {op:04b}, a: {a_src}, b: {b_src}, res: {res}, expected: {expected}")
 
 
 def test_case1(request):
@@ -46,6 +48,7 @@ def test_case1(request):
         waveform_filename = "report/mul.fst",
         coverage_filename = "report/mul.dat"
     )
+    dut.init_clock("clock")
     mlvp.run(mul_test(dut))
     dut.finalize()
     rp.set_line_coverage(request, "report/mul.dat")
