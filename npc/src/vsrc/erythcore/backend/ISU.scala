f703c121a8bbb7d27ef7bc85231b3cec533d8d08
yzcc <yangzechen21@mails.ucas.ac.cn>
2024-06-05 22:37:59 +0800

    add ISU

--- npc/src/vsrc/erythcore/backend/ISU.scala
+++ npc/src/vsrc/erythcore/backend/ISU.scala
@@ -13,5 +13,27 @@ import erythcore._
 class ISU extends Module with HasErythDefault{
     val io = IO(new Bundle {
         val in = Decoupled(Input(new InstCtrlBlk))
+
+        val out = Vec(2, Decoupled(Output(new InstCtrlBlk)))
+
+        val bypass  = Input(Vec(2, new BypassBundle))
     })
+
+    val intRS = Module(new RS)
+    val memRS = Module(new RS)
+
+    val instblk = io.in.bits
+
+    val use_int = instblk.basicInfo.instValid & instblk.basicInfo.fuType =/= FuType.lsu
+    val use_mem = instblk.basicInfo.instValid & instblk.basicInfo.fuType === FuType.lsu
+
+    intRS.io.enq.valid  := use_int
+    intRS.io.enq.bits   := instblk
+    intRS.io.bypass     := io.bypass
+    intRS.io.deq    <> io.out(0)
+
+    memRS.io.enq.valid  := use_mem
+    memRS.io.enq.bits   := instblk
+    memRS.io.bypass     := io.bypass
+    memRS.io.deq    <> io.out(1)
 }
